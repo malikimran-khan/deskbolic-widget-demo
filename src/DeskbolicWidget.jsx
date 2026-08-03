0377718dd54912bc8136c6d13574f1936f1079fb
imrantech90 <muhammad.imran@techbolic.com>
2026-08-03 00:16:02 -0700

    chore : change url option

--- src/DeskbolicWidget.jsx
+++ src/DeskbolicWidget.jsx
@@ -1,22 +1,29 @@
 import { useEffect } from 'react'
 
 const DESKBOLIC_SCRIPT_ID = 'deskbolic-widget-script'
-const DESKBOLIC_SCRIPT_SRC =
-  'https://widget.deskbolic.com/w/20848216-f418-4142-908b-e551bd0bd467/07119e89-487c-4034-9799-d49ec986fac4/embed.js'
+const DESKBOLIC_ELEMENT_IDS = ['deskbolic-widget-container', 'deskbolic-bubble']
 
-function DeskbolicWidget() {
+function DeskbolicWidget({ url }) {
   useEffect(() => {
-    if (document.getElementById(DESKBOLIC_SCRIPT_ID)) return
+    if (!url) return
+
+    document.getElementById(DESKBOLIC_SCRIPT_ID)?.remove()
+    DESKBOLIC_ELEMENT_IDS.forEach((id) => document.getElementById(id)?.remove())
 
     const script = document.createElement('script')
     script.id = DESKBOLIC_SCRIPT_ID
-    script.src = DESKBOLIC_SCRIPT_SRC
+    script.src = url
     script.async = true
     script.setAttribute('data-locale', 'en')
     script.setAttribute('data-position', 'right')
 
     document.body.appendChild(script)
-  }, [])
+
+    return () => {
+      script.remove()
+      DESKBOLIC_ELEMENT_IDS.forEach((id) => document.getElementById(id)?.remove())
+    }
+  }, [url])
 
   return null
 }
